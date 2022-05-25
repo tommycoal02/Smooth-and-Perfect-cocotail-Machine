dacae8adb6960e11760f333a705eb249b433f8b9
tomiwa abiola <tomiwabiola02@gmail.com>
2022-05-25 22:43:59 +0800

    First save of my coctail making machine

--- template/js/main.js
+++ template/js/main.js
@@ -16,6 +16,7 @@ function coctailDrinks(){
         .then(data => {
             console.log(data.drinks[0])
             document.querySelector('img').src= data.drinks[0].strDrinkThumb
+            document.querySelector('h3').innerText= data.drinks[0].strInstructions
 
         })
         .catch(err =>{
